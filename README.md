cf249c6fe4cf01cd3d6a81c74844d776d4847248
Stéphane Dauguet <sdauguet@gmail.com>
2021-05-12 18:33:18 +0200

    pagination for Characters

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Back for react application Marvel
+
+## Link
+
+[App Marvel Back](https://marvel-back-sda.herokuapp.com/comics)
--- routes/character.js
+++ routes/character.js
@@ -3,11 +3,15 @@ const axios = require("axios");
 const router = express.Router();
 
 const KEY = process.env.PRIVATE_KEY;
+const LIMIT = 100;
 
 router.get("/characters", async (req, res) => {
   try {
+    const page = req.query.page;
+    const skip = LIMIT * (page - 1);
+    console.log("page", page);
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${KEY}&limit=${LIMIT}&skip=${skip}`
     );
     res.json(response.data);
   } catch (error) {
